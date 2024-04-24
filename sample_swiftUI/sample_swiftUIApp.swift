721c619c292acc1682fff853098141d262f51d59
anhnguyenmta1318 <anhnguyenmta1318@gmail.com>
2024-04-24 14:43:55 +0700

    init swiftUI project

---
+++ sample_swiftUI/sample_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_swiftUIApp.swift
+//  sample_swiftUI
+//
+//  Created by anh.nguyen3 on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct sample_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
